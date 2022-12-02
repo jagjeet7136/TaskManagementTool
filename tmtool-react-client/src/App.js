6908d1cb4ac7c12f615dfdcd1f934c9a4816e6a4
Jagjeet Singh <jagjeet7136@gmail.com>
2022-12-02 19:16:11 +0530

    first-functional-component

--- tmtool-react-client/src/App.js
+++ tmtool-react-client/src/App.js
@@ -1,14 +1,22 @@
-import './App.css';
-import Dashboard from './components/Dashboard';
-import Header from './components/layout/Header';
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import Header from "./components/layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AddProject from "./components/project/AddProject";
 
 function App() {
   return (
-    <div className="App">
-    <Header/>
-      <Dashboard/>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Dashboard />
+        <Routes>
+          <Route exact path="/dashboard" element={Dashboard} />
+          <Route exact path="/addProject" element={AddProject} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
